3fd5ced78626dc525619b89f5dd067250e06fc18
Ben Hammerslag <b.hammerslag@gmail.com>
2017-04-22 14:48:19 -0500

    Full navbar
    * Generalized jQuery
    * All buttons

--- scripts.js
+++ scripts.js
@@ -2,24 +2,23 @@ $( document ).ready(function() {
 
   $.fn.extend({
     animateCss: function (animationName) {
-        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        this.addClass('animated ' + animationName).one(animationEnd, function() {
-            $(this).removeClass('animated ' + animationName);
-        });
+      var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+      this.addClass('animated ' + animationName).one(animationEnd, function() {
+        $(this).removeClass('animated ' + animationName);
+      });
     }
-});
-
-
-  $(".jumbotron h1").css("color", "red");
-  $("#contact-button").click(function(){
-    $("#contact-info").animateCss("tada");
-
   });
 
-  $("#projects-button").click(function(){
-    $("#project-info").animateCss("tada");
-
+  //Let's write pseudocode, like the old days!
+  /*
+  * Code should activate whenever *any* button in the nav is pressed
+  * When button is pressed, grab some info indicating the div to animate
+  * Then do it
+  */
+  $(".navbar-btn a").click(function(){
+    //Debug print stand-in:
+    //alert($(this).attr("href"));
+    $($(this).attr("href")).animateCss("tada");
   });
-
 
 });
